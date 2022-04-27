09eb0c2b6d3479726e9008bd56855885cc74cfae
Bryan Carpio <bryan_msn@hotmail.it>
2022-04-28 00:19:21 +0200

    💄 Product: new product card component

---
+++ components/product/Card.vue
@@ -0,0 +1,41 @@
+<!--<nuxt-link :to="`/products/${product.id}`">-->
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      Title product
+    </v-card-title>
+
+    <v-card-subtitle>
+      subtitle product
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+      >
+        see more
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
