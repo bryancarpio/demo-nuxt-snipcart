7031555ec36f2da884d28a50acbb6c2ed6b6f8d5
Bryan Carpio <bryan_msn@hotmail.it>
2022-06-07 17:15:08 +0200

    ✨ siteUrl ENV data

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,6 +81,10 @@ export default {
     ]
   },
 
+  env: {
+    siteUrl: process.env.SITE_URL || "http://localhost:3000"
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
