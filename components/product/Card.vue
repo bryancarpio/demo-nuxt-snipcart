f19ba947996294314507f7034b9bbd0c575c3222
Bryan Carpio <bryan_msn@hotmail.it>
2022-06-24 23:56:41 +0200

    💄 Card: dynamic image of product and price has been added

--- components/product/Card.vue
+++ components/product/Card.vue
@@ -4,7 +4,7 @@
     max-width="344"
   >
     <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      :src="product.image"
       height="200px"
     ></v-img>
 
@@ -16,15 +16,19 @@
       {{ product.description }}
     </v-card-subtitle>
 
+    <v-card-text large class="text-h6">
+      Price: <span class="font-weight-bold yellow--text">{{ product.price }} €</span>
+    </v-card-text>
+
     <v-card-actions>
-      <nuxt-link :to="`/products/${product.id}`">
-        <v-btn
-          color="orange lighten-2"
-          text
-        >
-          see more
-        </v-btn>
-      </nuxt-link>
+      <v-btn
+        color="orange lighten-2"
+        text
+        nuxt
+        :to="`/products/${product.id}`"
+      >
+        see more
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
